49cba76918f54bb58cf7133b5af4ef8a5c61ed97
zt515 <kiva515@foxmail.com>
2016-02-08 19:24:10 +0800

    Update Http::urlEncode

--- src/Http.cc
+++ src/Http.cc
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include <sstream>
 
 #include "Http.h"
@@ -18,20 +20,35 @@ Http::~Http()
 }
 
 
-std::string Http::urlEncode(const std::string &s)
+std::string Http::urlEncode(const std::string &str)
 {
-	std::string str = s;
-	int pos = 0;
-	
-	do {
-		pos = str.find(" ",pos);
-		if (pos != std::string::npos) {
-			str[pos] = '%';
-			str.insert(pos+1, "20");
+	std::stringstream ss;
+
+	size_t length = str.length();
+	unsigned char ch;
+	char buf[4];
+
+	for (size_t i = 0; i < length; i++) {
+		ch = str[i];
+
+		if (((ch >= 'A') && (ch < 'Z')) ||
+			((ch >= 'a') && (ch < 'z')) ||
+			((ch >= '0') && (ch < '9'))) {
+			ss << ch;
+
+		} else if (ch == ' ') {
+			ss << '+';
+
+		} else if (ch == '.' || ch == '-' || ch == '_' || ch == '*') {
+			ss << ch;
+
+		} else {
+			snprintf(buf, 4, "%%%02X", ch);
+			ss << buf;
 		}
-	} while(pos != std::string::npos);
-	
-	return std::move(s);
+	}
+
+	return std::move(ss.str());
 }
 
 
